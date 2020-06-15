be486bf56973732aaab2c4e7e816c097d8636e3a
LBB <3291282554@qq.com>
2020-06-15 14:00:29 +0800

    1

---
+++ admin/js/index.js
@@ -0,0 +1,17 @@
+$(function () {
+    $('.logout').on('click', function () {
+        alert('账号已登出');
+        localStorage.removeItem('token');
+        location.href = './login.html';
+    });
+
+    $.ajax({
+        type: 'get',
+        url: 'http://localhost:8080/api/v1/admin/user/info',
+        success: function (backData) {
+            console.log(backData);
+            $('.user_info img').attr('src', backData.data.userPic).next().find('i').text(backData.data.nickname);
+            $('.user_center_link img').attr('src', backData.data.userPic);
+        },
+    });
+})
